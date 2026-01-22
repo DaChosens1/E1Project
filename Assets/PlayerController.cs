e872b34779137756ef951cab44164235f0450603
DaChosens1 <48633888+DaChosens1@users.noreply.github.com>
2026-01-21 17:25:05 -0800

    Up to classwork 1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,20 +6,49 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float movementX;
     float movementY;
-    
+
     [SerializeField] float speed = 5.0f;
+    Rigidbody2D rb;
+    bool isGrounded;
+    int score = 0;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementDistanceX = movementX * speed * Time.deltaTime;
-        float movementDistanceY = movementY * speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        // float movementDistanceX = movementX * speed * Time.deltaTime;
+        // float movementDistanceY = movementY * speed * Time.deltaTime;
+        // transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        rb.linearVelocity = new Vector2(movementX*speed, rb.linearVelocity.y);
+        if (isGrounded && movementY > 0)
+        {
+            rb.AddForce(new Vector2(0, 100));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            score++;
+            collision.gameObject.SetActive(false);
+            Debug.Log("Score: " + score);
+        }
     }
 
     void OnMove(InputValue value) {
@@ -30,3 +59,4 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Movement Y = " + movementY);
     }
 }
+
